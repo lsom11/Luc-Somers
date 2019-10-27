b62966ad8c04a2d7c6094ab9f27643b410866071
Luc Somers <lucwsomers@gmail.com>
2019-10-27 01:36:26 -0300

    Updates

--- sw.js
+++ sw.js
@@ -26,24 +26,20 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-fa70112f1e5e43d09e4a.js"
+    "url": "webpack-runtime-04e5db32cc78b94ef96f.js"
   },
   {
     "url": "commons-edb40c1dccc2c8b3aff4.js"
   },
   {
-    "url": "app-18ff0c42e7d0d569eeb5.js"
+    "url": "app-fcc34a13b1efeeddf2bf.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-a7388efdc62430243a75.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "4cc98fd065f42510d311e25ecb392600"
-  },
-  {
-    "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
-    "revision": "d274adf0f008ef152ce70a312b04b730"
+    "revision": "fb65c39b3ca33baafb8cb2271f11860f"
   },
   {
     "url": "manifest.webmanifest",
@@ -66,12 +62,12 @@ const { NavigationRoute } = workbox.routing
 
 const navigationRoute = new NavigationRoute(async ({ event }) => {
   let { pathname } = new URL(event.request.url)
-  pathname = pathname.replace(new RegExp(`^/portfolio`), ``)
+  pathname = pathname.replace(new RegExp(`^`), ``)
 
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/portfolio/app-18ff0c42e7d0d569eeb5.js`))) {
+  if (!resources || !(await caches.match(`/app-fcc34a13b1efeeddf2bf.js`))) {
     return await fetch(event.request)
   }
 
@@ -84,7 +80,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
     }
   }
 
-  const offlineShell = `/portfolio/offline-plugin-app-shell-fallback/index.html`
+  const offlineShell = `/offline-plugin-app-shell-fallback/index.html`
   return await caches.match(offlineShell)
 })
 
